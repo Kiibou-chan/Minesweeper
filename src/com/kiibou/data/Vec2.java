01dbcddb6b1c406bfc9d43adaf89375bf5a0d447
Svenja Kernig <svenja.kernig@gmx.de>
2019-10-30 07:20:42 +0100

    add Vec2, Vec3

---
+++ src/com/kiibou/data/Vec2.java
@@ -0,0 +1,13 @@
+package com.kiibou.data;
+
+public class Vec2 {
+
+    public final int x;
+    public final int y;
+
+    public Vec2(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
---
+++ src/com/kiibou/data/Vec3.java
@@ -0,0 +1,11 @@
+package com.kiibou.data;
+
+public class Vec3 extends Vec2 {
+
+    public final int z;
+
+    public Vec3(final int x, final int y, final int z) {
+        super(x, y);
+        this.z = z;
+    }
+}
