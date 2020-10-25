2efad52d9bd12b0eef903fe3a0bb5c9fe00d7161
Svenja Kernig <svenja.kernig@gmx.de>
2020-10-25 15:36:35 +0100

    describe possible future additions to vector package

--- GUILib/src/space/kiibou/gui/vector/VectorGraphics.kt
+++ GUILib/src/space/kiibou/gui/vector/VectorGraphics.kt
@@ -8,6 +8,40 @@ import space.kiibou.gui.GraphicsElement
 
 typealias GraphicsElementFactory = (GApplet) -> GraphicsElement
 
+/* TODO: 03/11/2019 \
+ * Primitives:
+ * arc (x, y, w, h, angle: start [radians], angle: stop [radians], mode { PIE, OPEN, CHORD })
+ * circle (x, y, extent)
+ * ellipse (x, y, w, h)
+ * point (x, y)
+ * quad (x1, y1, x2, y2, x3, y3, x4, y4)
+ * square (x, y, extent)
+ * triangle (x1, y1, x2, y2, x3, y3)
+ *
+ * Curves:
+ * bezier (x1, y1, x2, y2, x3, y3, x4, y4)
+ * curve (x1, y1, x2, y2, x3, y3, x4, y4)
+ *  curveTightness (tightness: [-5..5])
+ *
+ * Modes:
+ * ellipseMode (mode { RADIUS, CENTER, CORNER, CORNERS })
+ * rectMode (mode { RADIUS, CENTER, CORNER, CORNERS })
+ *
+ * Vertex:
+ * contour -> beginContour, endContour
+ * bezierVertex (cx2, cy2, cx3, cy3, x4, y4)
+ * curveVertex (x, y)
+ * quadraticVertex (cx, cy, x, y)
+ *
+ * Transforms:
+ * translate
+ * rotate
+ * scale
+ * apply matrix?
+ *
+ * Add Events and Animations?
+ */
+
 class VectorGraphics(app: GApplet, x: Int, y: Int, width: Int, height: Int, scale: Int, dataPath: String) : GraphicsElement(app, x, y, width, height) {
     private val data = mapper.readTree(VectorGraphics::class.java.classLoader.getResourceAsStream(dataPath))
 
